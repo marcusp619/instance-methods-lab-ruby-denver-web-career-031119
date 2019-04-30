bb0ee04c7e1e047316ea706092588c21a2e020b6
Mark Pearyer <marcusp619@gmail.com>
2019-04-30 16:07:43 +0000

    Automatically backed up by Learn

--- lib/person.rb
+++ lib/person.rb
@@ -1,10 +1,10 @@
 class Person
   def talk
-    put "Hello World!"
+    puts "Hello World!"
   end
   
   def walk 
-    put "The Person is walking"
+    puts "The Person is walking"
   end
   
 end 
